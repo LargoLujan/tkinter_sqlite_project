25a8cba9c63097b91b05be581f8ba867262ab4b6
Manuel Luján Vilchez <miveritas.vivir@gmail.com>
2023-03-26 01:49:41 +0100

    add validation method

--- app.py
+++ app.py
@@ -4,14 +4,7 @@ import sqlite3
 
 
 class Product:
-    db = 'database/product.db'
-
-    def db_query(self, consulta, parametros=()):
-        with sqlite3.connect(self.db) as con:
-            cursor = con.cursor()
-            resultado = cursor.execute(consulta, parametros)
-            con.commit()
-        return resultado
+    db = 'database/products.db'
 
     def __init__(self, root):
         self.windows = root
@@ -32,29 +25,70 @@ class Product:
         self.nombre.grid(row=1, column=1)
 
         # Label Precio
-        self.etiqueta_precio = Label(frame, text='precio: ')
+        self.etiqueta_precio = Label(frame, text='Precio: ')
         self.etiqueta_precio.grid(row=2, column=0)
         # Entry Precio
         self.precio = Entry(frame)
         self.precio.grid(row=2, column=1)
 
         # Botón añadir Producto
-        self.boton_add = ttk.Button(frame, text='Guardar Producto')
+        self.boton_add = ttk.Button(frame, text='Guardar Producto', command=self.add_product)
         self.boton_add.grid(row=3, columnspan=2, sticky=W + E)
 
         # Tabla Productos
         # Estilo personalizado para la tabla
         style = ttk.Style()
-        style.configure("mystyle.Treeview", highlightthickness=0, bd=0, font=('Calibri',11)) # Se modifica la fuente de la tabla
-        style.configure("mystyle.Treeview.Heading", font=('Calibri', 13, 'bold')) # Se modifica la fuente de las cabeceras
-        style.layout("mystyle.Treeview", [('mystyle.Treeview.treearea', {'sticky':'nswe'})])  # Eliminamos los bordes
+        style.configure("mystyle.TreeView", highlightthickness=0, bd=0,
+                        font=('Calibri', 11))  # Se modifica la fuente de la tabla
+        style.configure("mystyle.TreeView.Heading",
+                        font=('Calibri', 13, 'bold'))  # Se modifica la fuente de las cabeceras
+        style.layout("mystyle.TreeView", [('mystyle.TreeView.treearea', {'sticky': 'nswe'})])  # Eliminamos los bordes
 
         # Estructura de la tabla
         self.tabla = ttk.Treeview(frame, height=20, columns=2, style='mystyle.TreeView')
         self.tabla.grid(row=4, column=0, columnspan=2)
-        self.tabla.heading('#0', text='Nombre', anchor=CENTER)
+        self.tabla.heading('#0', text='Nombre', anchor=CENTER)  # Cabeceras de la tabla
         self.tabla.heading('#1', text='Precio', anchor=CENTER)
 
+    def db_query(self, consulta, parametros=()):
+        with sqlite3.connect(self.db) as con:
+            cursor = con.cursor()
+            resultado = cursor.execute(consulta, parametros)
+            con.commit()
+        return resultado
+
+    def get_products(self):
+
+        registros_tabla = self.tabla.get_children()
+        for fila in registros_tabla:
+            self.tabla.delete(fila)
+
+        query = "SELECT * FROM product ORDER BY name DESC"
+        registros = self.db_consulta(query)
+        print(registros)
+
+        for fila in registros:
+            print(fila)
+            self.tabla.insert("", 0, text=fila[1], values=fila[2])
+
+    def validacion_nombre(self):
+        nombre_introducido_por_usuario = self.nombre.get()
+        return len(nombre_introducido_por_usuario) != 0
+
+    def validacion_precio(self):
+        precio_introducido_por_usuario = self.precio.get()
+        return len(precio_introducido_por_usuario) != 0
+
+    def add_product(self):
+        if self.validacion_nombre() and self.validacion_precio():
+            print(self.nombre.get())
+            print(self.precio.get())
+        elif self.validacion_nombre() and self.validacion_precio() == False:
+            print("El precio es obligatorio")
+        elif self.validacion_precio() and self.validacion_nombre() == False:
+            print("El nombre es obligatorio")
+        else:
+            print("El nombre y el precio son obligatorios")
 
 
 if __name__ == '__main__':
